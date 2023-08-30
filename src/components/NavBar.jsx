199a85006acb78fc51e5a23112c8cb57ec7dddf6
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-08-30 17:51:02 +0100

    a

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,7 +22,7 @@ export default function NavBar(props) {
                     {/* <MenuToggle toggle={() => toggleOpen()} /> */}
                     <Link legacyBehavior href={'/'}>
                         <a>
-                            <Logo height='70%' hover />
+                            <Logo height='170%' hover />
                         </a>
                     </Link>
                 </div>
