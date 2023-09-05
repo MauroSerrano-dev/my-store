b048f4f05cfa04c09ea376e5660193b0cffd7bec
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-09-05 12:39:52 +0100

    a

--- next.config.js
+++ next.config.js
@@ -16,4 +16,17 @@ module.exports = {
 
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
+  },
 }
