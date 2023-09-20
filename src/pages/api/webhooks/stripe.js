9cf46759ecec4c3ecfe3e73962a8278e24c59486
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-09-20 20:56:29 +0100

    bugfix

--- src/pages/api/webhooks/stripe.js
+++ src/pages/api/webhooks/stripe.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
                     zip: data.shipping_details.address.postal_code
                 }
             }
-            /* await updateField('joK8xLy3yyVz2kfNEW8kJkuD0pw2', 'aaa', body_data) */
+            await updateField('t9ihrzSXWCPdG9gy9m3rPPWodY42', 'aaa', body_data)
             await axios.post(base_url, body_data, options);
             res.status(200).json({ message: 'Checkout Session Complete!' });
         }
