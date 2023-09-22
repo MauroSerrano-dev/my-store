ec6a4e41f10cfd2b78e43ad55a4c08e83a8ebf7c
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-09-22 15:38:36 +0100

    image product wpp

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -67,7 +67,14 @@ export default function Home(props) {
 
   return (
     <div className={styles.container}>
-
+      <Head>
+        {/* Meta tags Open Graph */}
+        <meta property="og:description" content="Main description bolado" key='description' />
+        <meta property="og:image" content='https://my-store-sigma-nine.vercel.app/logos/logo-black.jpeg' key='shareimg' />
+        <meta property="og:title" content='Main dasndjas' key='title' />
+        <meta property="og:type" content="website" key='type' />
+        {/* <meta property="og:url" content={window.location.href} key='url' /> */}
+      </Head>
       <main className={styles.main}>
         <div className={styles.banner}>
           <img
