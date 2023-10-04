4f4e3ee060af95d9998d319f3cec326b72bb1e0c
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-10-04 02:10:04 +0100

    page error test

--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -1,26 +1,10 @@
-import styles from '@/styles/_error.module.css'
-
 function Error({ statusCode }) {
     return (
-        <div
-            className="fillWidth flex center"
-            style={{
-                '--text-color': 'var(--text-white)'
-            }}
-        >
-            <p style={{ fontWeight: 'bold', fontSize: '24px' }}>
-                {statusCode
-                    ? `An error ${statusCode} occurred on server`
-                    : 'An error occurred on client'
-                }
-            </p>
-            <Link
-                href="/"
-                className={styles.link}
-            >
-                Back to homepage
-            </Link>
-        </div>
+        <p>
+            {statusCode
+                ? `An error ${statusCode} occurred on server`
+                : 'An error occurred on client'}
+        </p>
     )
 }
 
