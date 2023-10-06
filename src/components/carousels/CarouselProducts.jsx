3b9ece38611f38788b95166bb3353dcb08d96175
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-10-06 07:33:52 +0100

    carousel

--- src/components/carousels/CarouselProducts.jsx
+++ src/components/carousels/CarouselProducts.jsx
@@ -57,7 +57,7 @@ export default function CarouselProducts(props) {
                 initial={{ x: 0 }}
                 drag="x"
                 dragElastic={0.25}
-                dragTransition={{ power: 1, timeConstant: 200 }}
+                dragTransition={{ power: 0.7, timeConstant: 200 }}
                 onDragStart={handleDragStart}
                 onDragEnd={handleDragEnd}
                 whileTap={{ cursor: 'grabbing' }}
