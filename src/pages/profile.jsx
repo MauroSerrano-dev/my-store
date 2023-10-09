aa2406377b2944949938c3814183c9f0dc1c3111
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-10-09 05:52:46 +0100

    profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,16 +1,22 @@
 import styles from '@/styles/pages/profile.module.css'
 import Head from 'next/head'
+import NoFound404 from './404'
 
 export default function Profile(props) {
     const {
+        session
     } = props
 
     return (
-        <div className={styles.container}>
-            <Head>
-            </Head>
-            <div className={styles.productContainer}>
-            </div>
-        </div>
+        session === undefined
+            ? <div></div>
+            : session === null
+                ? <NoFound404 />
+                : <div className={styles.container}>
+                    <Head>
+                    </Head>
+                    <div className={styles.productContainer}>
+                    </div>
+                </div>
     )
 }
