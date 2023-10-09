af6b56165d4c89098d7abb02c9dca5c016cfe515
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-10-09 04:23:48 +0100

    bugFix Product

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -89,7 +89,7 @@ export default function Product(props) {
     }, [isImageLoaded])
 
     return (
-        show && isImageLoaded &&
+        show &&
         <motion.div
             className={styles.container}
             variants={motionVariants}
