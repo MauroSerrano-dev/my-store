4f632d0aa6d9b9fd3d092b0b1f18d51c98666c3b
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-10-13 06:38:37 +0100

    new slide

--- src/components/ImagesSlider.jsx
+++ src/components/ImagesSlider.jsx
@@ -114,7 +114,7 @@ export default function ImagesSlider(props) {
                     transition={{ type: 'spring', stiffness: 120, damping: 25 }}
                     drag="x"
                     dragElastic={0.25}
-                    dragTransition={{ power: supportsHoverAndPointer ? 0.035 : 0.15, timeConstant: 200 }}
+                    dragTransition={{ power: supportsHoverAndPointer ? 0.04 : 0.2, timeConstant: 200 }}
                     onDragStart={handleDragStart}
                     onDragEnd={handleDragEnd}
                     onMouseDown={handleMouseDown}
