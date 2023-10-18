697f8131c2d4c90966919ae0ee2eb3edcbf08b8a
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-10-18 14:52:32 +0100

    printifywebhook

--- src/pages/api/webhooks/stripe.js
+++ src/pages/api/webhooks/stripe.js
@@ -70,7 +70,7 @@ export default async function handler(req, res) {
             }
 
             const printifyRes = await axios.post(base_url, body_data, options)
-            await createTest({ printifyRes: printifyRes ? JSON.stringify(printifyRes) : 'ana' })
+            await createTest({ printifyRes: printifyRes ? JSON.stringify(printifyRes.body) : 'ana' })
             if (is_loggin)
                 await updateCart(cart_id, [])
             else
