0a8ffbec2dff66d11c37d1fa0380018eed97625c
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-10-31 22:15:28 +0000

    cron squedule

--- src/pages/api/users-clear-update-counter.js
+++ src/pages/api/users-clear-update-counter.js
@@ -2,15 +2,17 @@ import { isTokenValid } from "../../../auth"
 import { clearUpdateCounter } from "../../../backend/user"
 
 export default async function handler(req, res) {
-    const { authorization } = req.headers.authorization
+    const { authorization } = req.headers
 
     if (!authorization) {
         console.error("Authentication token not provided.")
         return res.status(401).json({ error: "Authentication token not provided." })
     }
 
-    if (!isTokenValid(authorization, process.env.CRON_SECRET))
+    if (!isTokenValid(authorization, process.env.CRON_SECRET)) {
+        console.error("Invalid authentication token.")
         return res.status(401).json({ error: "Invalid authentication token." })
+    }
 
     const response = await clearUpdateCounter()
     res.status(response.status).json(response)
