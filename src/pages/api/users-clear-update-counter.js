d9e0b003226a892113cc1fbec9f4c7e47848ae3d
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-10-31 21:37:05 +0000

    cron squedule

--- src/pages/api/users-clear-update-counter.js
+++ src/pages/api/users-clear-update-counter.js
@@ -2,7 +2,7 @@ import { isTokenValid } from "../../../auth"
 import { clearUpdateCounter } from "../../../backend/user"
 
 export default async function handler(req, res) {
-    const { authorization } = req.headers.authorization.split('Bearer ').at(1)
+    const { authorization } = req.headers.authorization
 
     if (!authorization)
         return res.status(401).json({ error: "Authentication token not provided." })
