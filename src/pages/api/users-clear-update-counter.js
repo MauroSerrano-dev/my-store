a91194aa453b68158eb5e28bf1520dffedb8acd8
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-10-31 22:03:46 +0000

    cron squedule

--- src/pages/api/users-clear-update-counter.js
+++ src/pages/api/users-clear-update-counter.js
@@ -1,20 +1,15 @@
-import { isTokenValid } from "../../../auth"
-import { clearUpdateCounter } from "../../../backend/user"
+import { NextResponse } from 'next/server';
 
-export default async function handler(req, res) {
-    /* const { authorization } = req.headers.authorization */
-    const authToken = (req.headers.get('authorization') || '')
-        .split('Bearer ')
-        .at(1)
-    await clearUpdateCounter(authToken)
+export default function handler(request) {
+    const authHeader = request.headers.get('authorization');
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+        return NextResponse.json(
+            { success: false },
+            {
+                status: 401,
+            },
+        );
+    }
 
-    /* if (!authorization)
-        return res.status(401).json({ error: "Authentication token not provided." })
-
-    if (!isTokenValid(authorization, process.env.CRON_SECRET))
-        return res.status(401).json({ error: "Invalid authentication token." })
-
-    const response = await clearUpdateCounter()
-    res.status(response.status).json(response) */
-    res.status(200).json({})
+    return NextResponse.json({ success: true });
 }
