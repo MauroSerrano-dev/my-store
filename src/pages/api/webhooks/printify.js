9c5a375ec2864ef5c6e1990545c8fb316e0c32eb
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-11-03 15:50:33 +0000

    checkout

--- src/pages/api/webhooks/printify.js
+++ src/pages/api/webhooks/printify.js
@@ -31,7 +31,7 @@ async function createWeebhook(body) {
 export default async function handler(req, res) {
     if (req.method === "POST") {
         const body = req.body
-        /* const response = await createWeebhook(body) */
-        res.status(200).json({ /* message: response.message */ })
+        const response = await createWeebhook(body)
+        res.status(200).json({ message: response.message })
     }
 }
