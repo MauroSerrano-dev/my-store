ef823e7c99127a0ae4bb74c7eea4a536316df610
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-11-03 18:10:10 +0000

    checkout

--- src/pages/api/webhooks/stripe.js
+++ src/pages/api/webhooks/stripe.js
@@ -118,8 +118,8 @@ export default async function handler(req, res) {
     }
 }
 
-/* export const config = {
+export const config = {
     api: {
-        bodyParse: false,
+        bodyParser: false,
     },
-} */
+}
