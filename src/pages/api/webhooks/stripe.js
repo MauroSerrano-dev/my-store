5ef87edf42a5b0403f77235a8ea1197f86c0c39d
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-11-03 18:12:18 +0000

    checkout

--- src/pages/api/webhooks/stripe.js
+++ src/pages/api/webhooks/stripe.js
@@ -114,7 +114,7 @@ export default async function handler(req, res) {
             }
         }
     } catch (error) {
-        res.status(500).json({ error: 'Error on stripe webhook' })
+        res.status(500).json({ error: `Error on stripe webhook: ${error}` })
     }
 }
 
