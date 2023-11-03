a7c98dec63442d2e0decfa8cbbe5de4ea28d79ba
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-11-03 18:28:01 +0000

    checkout

--- src/pages/api/webhooks/stripe.js
+++ src/pages/api/webhooks/stripe.js
@@ -12,114 +12,113 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 export default async function handler(req, res) {
     const sig = req.headers['stripe-signature']
 
+    let event
+
     try {
-        const body = req.body
-        const rawBody = body.toString()
-        stripe.webhooks.constructEvent(rawBody, sig, process.env.STRIPE_WEBHOOK_SECRET)
+        const rawBody = await getRawBody(req)
+        event = stripe.webhooks.constructEvent(rawBody, sig, process.env.STRIPE_WEBHOOK_SECRET)
     }
     catch (error) {
         return res.status(401).json({ error: `Invalid authentication. ${error}` })
     }
 
     try {
-        if (req.method === "POST") {
-            const type = req.body.type
-            const data = req.body.data.object
+        const type = event.type
+        const data = event.data.object
 
-            if (type === 'checkout.session.completed') {
-                const base_url = `https://api.printify.com/v1/shops/${process.env.PRINTIFY_SHOP_ID}/orders.json`
+        if (type === 'checkout.session.completed') {
+            const base_url = `https://api.printify.com/v1/shops/${process.env.PRINTIFY_SHOP_ID}/orders.json`
 
-                const items = data.metadata
+            const items = data.metadata
 
-                const cart_id = JSON.parse(items.cart_id)
-                const is_loggin = JSON.parse(items.is_loggin)
+            const cart_id = JSON.parse(items.cart_id)
+            const is_loggin = JSON.parse(items.is_loggin)
 
-                delete items.cart_id
-                delete items.is_loggin
+            delete items.cart_id
+            delete items.is_loggin
 
-                const line_items = Object.keys(items).map(key => JSON.parse(items[key]))
+            const line_items = Object.keys(items).map(key => JSON.parse(items[key]))
 
-                const orderId = uuidv4()
+            const orderId = uuidv4()
 
-                const options = {
-                    headers: {
-                        Authorization: process.env.PRINTIFY_ACCESS_TOKEN,
-                        'Content-Type': 'application/json',
-                    },
-                }
+            const options = {
+                headers: {
+                    Authorization: process.env.PRINTIFY_ACCESS_TOKEN,
+                    'Content-Type': 'application/json',
+                },
+            }
 
-                const body_data = {
-                    external_id: orderId,
-                    label: `Order: ${orderId}`,
-                    line_items: line_items.map(lineItem => (
-                        {
-                            product_id: lineItem.id_printify,
-                            variant_id: lineItem.variant_id,
-                            quantity: lineItem.quantity,
-                        }
-                    )),
-                    shipping_method: 1,
-                    send_shipping_notification: true,
-                    address_to: {
-                        first_name: data.customer_details.name,
-                        last_name: "Serrano",
-                        email: data.customer_details.email,
-                        phone: data.customer_details.phone,
-                        country: data.shipping_details.address.country,
-                        region: data.shipping_details.address.state === '' ? data.shipping_details.address.city : data.shipping_details.address.state,
-                        address1: data.shipping_details.address.line1,
-                        address2: data.shipping_details.address.line2,
-                        city: data.shipping_details.address.city,
-                        zip: data.shipping_details.address.postal_code
-                    }
-                }
-
-                const printifyRes = await axios.post(base_url, body_data, options)
-
-                await createOrder(
+            const body_data = {
+                external_id: orderId,
+                label: `Order: ${orderId}`,
+                line_items: line_items.map(lineItem => (
                     {
-                        id: orderId,
-                        printify_id: printifyRes.data.id,
-                        customer: {
-                            ...data.customer_details,
-                        },
-                        shipping_details: {
-                            shipping_cost: data.shipping_cost,
-                            ...data.shipping_details,
-                        },
-                        products: line_items,
-                        stripe_id: data.id,
-                        currency: data.currency,
-                        amount_total: data.amount_total,
-                        amount_subtotal: data.amount_subtotal,
+                        product_id: lineItem.id_printify,
+                        variant_id: lineItem.variant_id,
+                        quantity: lineItem.quantity,
                     }
-                )
+                )),
+                shipping_method: 1,
+                send_shipping_notification: true,
+                address_to: {
+                    first_name: data.customer_details.name,
+                    last_name: "Serrano",
+                    email: data.customer_details.email,
+                    phone: data.customer_details.phone,
+                    country: data.shipping_details.address.country,
+                    region: data.shipping_details.address.state === '' ? data.shipping_details.address.city : data.shipping_details.address.state,
+                    address1: data.shipping_details.address.line1,
+                    address2: data.shipping_details.address.line2,
+                    city: data.shipping_details.address.city,
+                    zip: data.shipping_details.address.postal_code
+                }
+            }
 
-                if (is_loggin)
-                    await updateCart(cart_id, [])
-                else
-                    await updateCartSessionProducts(cart_id, [])
+            const printifyRes = await axios.post(base_url, body_data, options)
 
-                res.status(200).json({ message: `Order ${orderId} Created. Checkout Complete!` })
-            }
-            else if (type === 'checkout.session.async_payment_succeeded') {
-            }
-            else if (type === 'checkout.session.async_payment_failed') {
-            }
-            else if (type === 'checkout.session.expired') {
-                res.status(200).json({ message: 'Checkout Session Expired!' })
-            }
-            else {
-                res.status(200).json({ message: 'Outros eventos!' })
-            }
+            await createOrder(
+                {
+                    id: orderId,
+                    printify_id: printifyRes.data.id,
+                    customer: {
+                        ...data.customer_details,
+                    },
+                    shipping_details: {
+                        shipping_cost: data.shipping_cost,
+                        ...data.shipping_details,
+                    },
+                    products: line_items,
+                    stripe_id: data.id,
+                    currency: data.currency,
+                    amount_total: data.amount_total,
+                    amount_subtotal: data.amount_subtotal,
+                }
+            )
+
+            if (is_loggin)
+                await updateCart(cart_id, [])
+            else
+                await updateCartSessionProducts(cart_id, [])
+
+            res.status(200).json({ message: `Order ${orderId} Created. Checkout Complete!` })
+        }
+        else if (type === 'checkout.session.async_payment_succeeded') {
+        }
+        else if (type === 'checkout.session.async_payment_failed') {
+        }
+        else if (type === 'checkout.session.expired') {
+            res.status(200).json({ message: 'Checkout Session Expired!' })
+        }
+        else {
+            res.status(200).json({ message: 'Outros eventos!' })
         }
     } catch (error) {
         res.status(500).json({ error: `Error on stripe webhook: ${error}` })
     }
 }
 
-/* export const config = {
+export const config = {
     api: {
         bodyParser: false,
     },
-} */
+}
