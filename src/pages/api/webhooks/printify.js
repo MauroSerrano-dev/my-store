10242976c8667cf4be05b60476a69fae3ce22c28
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-11-05 19:25:27 +0000

    printify webhook

--- src/pages/api/webhooks/printify.js
+++ src/pages/api/webhooks/printify.js
@@ -35,35 +35,36 @@ export default async function handler(req, res) {
     await createWeebhook({ cu: 'cuuuuuuuuuuuuuuuu' })
     try {
         const calculatedSignature = CryptoJS.HmacSHA256(req.body, process.env.PRINTIFY_WEBHOOK_SECRET).toString(CryptoJS.enc.Base64)
+        await createWeebhook({ bb: calculatedSignature, cc: req.headers['x-pfy-signature'] })
 
         if (calculatedSignature !== req.headers['x-pfy-signature'])
             return res.status(401).json({ error: 'Invalid authentication.' })
         await createWeebhook({ aa: 'aaaaaaaaaaaaaaaaa' })
         await createWeebhook(req.headers)
 
-/*         if (req.method === "POST") {
-            const body = req.body
-            const type = body.type
-
-            const orderId = body.resource.id
-
-            const base_url = `https://api.printify.com/v1/shops/${process.env.PRINTIFY_SHOP_ID}/orders/${orderId}.json`
-
-            const options = {
-                headers: {
-                    Authorization: process.env.PRINTIFY_ACCESS_TOKEN,
-                },
-            }
-
-            if (type === 'order:updated' || type === 'order:sent-to-production' || type === 'order:shipment:created' || type === 'order:shipment:delivered') {
-
-                const orderRes = await axios.get(base_url, options)
-
-                await updateProductStatus(orderId, orderRes.data.line_items, { authorization: req.headers.authorization || null })
-
-                res.status(200).json({ message: 'Order status updated!' })
-            }
-        } */
+        /*         if (req.method === "POST") {
+                    const body = req.body
+                    const type = body.type
+        
+                    const orderId = body.resource.id
+        
+                    const base_url = `https://api.printify.com/v1/shops/${process.env.PRINTIFY_SHOP_ID}/orders/${orderId}.json`
+        
+                    const options = {
+                        headers: {
+                            Authorization: process.env.PRINTIFY_ACCESS_TOKEN,
+                        },
+                    }
+        
+                    if (type === 'order:updated' || type === 'order:sent-to-production' || type === 'order:shipment:created' || type === 'order:shipment:delivered') {
+        
+                        const orderRes = await axios.get(base_url, options)
+        
+                        await updateProductStatus(orderId, orderRes.data.line_items, { authorization: req.headers.authorization || null })
+        
+                        res.status(200).json({ message: 'Order status updated!' })
+                    }
+                } */
     }
     catch (error) {
         res.status(500).json({ error: `Error on printify webhook: ${error}` })
