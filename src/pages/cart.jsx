37b7d5a78447dff147405085ea5a9423d840244c
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-11-07 01:20:11 +0000

    api

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -49,7 +49,7 @@ export default function Cart(props) {
                     return {
                         ...prod,
                         id_printify: prod.printify_ids[providerId],
-                        variant_id: prod.variant_id,
+                        variant_id: prod.variant.id,
                         variant_id_printify: typeof prod.variant.id_printify === 'number' ? prod.variant.id_printify : prod.variant.id_printify[providerId],
                         price: convertDolarToCurrency(prod.variant.price, userCurrency.code),
                     }
