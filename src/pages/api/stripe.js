e9e9e57f8e2f11d66cbd9318c38cc0c1f82e3cd4
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-11-08 15:59:50 +0000

    stripe api

--- src/pages/api/stripe.js
+++ src/pages/api/stripe.js
@@ -41,11 +41,6 @@ export default async function handler(req, res) {
         })
       }
     }
-    else {
-      await stripe.customers.create({
-        name: 'Anonymous',
-      })
-    }
 
     const line_items = cartItems.map(item => {
       return {
