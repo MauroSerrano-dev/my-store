2f6bbe73d7a1e38a26a93fec8b4bdcde36390635
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-11-13 00:11:11 +0000

    test2

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -91,7 +91,7 @@ export default function Home(props) {
         <meta property="og:url" content='https://mrfstyles.com' key='og:url' />
       </Head>
       <main className={styles.main}>
-        <Link
+{/*         <Link
           className={`${styles.banner} noUnderline`}
           draggable={false}
           href='/search?c=sound-vibes'
@@ -132,7 +132,7 @@ export default function Home(props) {
             alt='t-shirts_colors'
             className={styles.bannerColors}
           />
-        </Link>
+        </Link> */}
         <div className={styles.infos}>
           {/* <div className={styles.infosItem}>
             <LocalShippingOutlinedIcon
