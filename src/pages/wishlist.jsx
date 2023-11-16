4e468dc61de2ca11a1429a27e937485c059a6188
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-11-16 00:42:14 +0000

    wishlist

---
+++ src/pages/wishlist.jsx
@@ -0,0 +1,17 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+export default function Wishlist() {
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export async function getServerSideProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ['common', 'menu', 'navbar']))
+        }
+    }
+}
