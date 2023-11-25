527916301016aa8adec4f1ff1bc41a2a800ff9e1
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-11-25 04:55:03 +0000

    verify email

--- src/pages/email-verification.jsx
+++ src/pages/email-verification.jsx
@@ -1,39 +1,60 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import Head from 'next/head'
 import { getAuth, applyActionCode } from 'firebase/auth';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { CircularProgress } from '@mui/material';
+import { showToast } from '../../utils/toasts';
 
 export default function EmailVerification(props) {
     const {
         router
     } = props
 
+    const [showSpinner, setShowSpinner] = useState(false)
+
     useEffect(() => {
-        // Verificar se há um código de verificação no URL
+        setShowSpinner(true)
         if (router.query?.oobCode) {
-            // Obtendo a instância de autenticação do Firebase
             const auth = getAuth()
 
-            // Use o código de verificação para verificar o e-mail
             applyActionCode(auth, router.query.oobCode)
                 .then(() => {
-                    // Email verificado com sucesso!
-                    // Atualize o status no seu aplicativo ou redirecione o usuário, se necessário
-                    console.log('E-mail verificado com sucesso!');
+                    showToast({ msg: 'E-mail verificado com sucesso!' })
+                    router.push('/')
                 })
-                .catch((error) => {
-                    // Trate os erros de verificação de e-mail aqui
-                    console.error('Erro ao verificar o e-mail:', error);
-                });
+                .catch(() => {
+                    showToast({ msg: 'Erro ao verificar o e-mail' })
+                    router.push('/')
+                })
         }
     }, [])
 
     return (
-        <div className='flex center column'>
-            <Head>
-            </Head>
-            <main>
-            </main>
+        <div className='flex center column fillWidth' style={{ justifyContent: 'flex-start', paddingTop: '7rem' }}>
+            {showSpinner &&
+                <div
+                    className='flex center column fillWidth'
+                >
+                    <CircularProgress
+                        variant="determinate"
+                        sx={{
+                            position: 'absolute',
+                            color: '#525252',
+                        }}
+                        size={60}
+                        thickness={4}
+                        value={100}
+                    />
+                    <CircularProgress
+                        disableShrink
+                        size={60}
+                        thickness={4}
+                        sx={{
+                            position: 'absolute',
+                            animationDuration: '750ms',
+                        }}
+                    />
+                </div>
+            }
         </div>
     )
 }
