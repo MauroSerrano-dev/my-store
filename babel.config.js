e42d4b1f77ce1faf1e68b2c66b63e6a28f71efe7
Mauro Serrano <mauro.serrano@hotmail.com.br>
2024-01-05 02:57:46 +0000

    muita coisa

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,8 @@
 module.exports = {
     presets: [
-        [
-            '@babel/preset-env',
-            {
-                targets: {
-                    node: 'current',
-                },
-            },
-        ],
+        '@babel/preset-env', // Add this line
+        '@babel/preset-react',
+        // ... any other presets you are using
     ],
+    // ... any other configurations
 };
