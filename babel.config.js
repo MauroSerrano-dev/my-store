6bb47d5eabcc1c05d069c9faff2e2cbc166af465
Mauro Serrano <mauro.serrano@hotmail.com.br>
2024-01-05 03:03:07 +0000

    bugfix

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     presets: [
+        'next/babel',
         '@babel/preset-env', // Add this line
         '@babel/preset-react',
         // ... any other presets you are using
